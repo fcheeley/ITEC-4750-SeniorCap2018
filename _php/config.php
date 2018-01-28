fe4409bc2078c8fbf4c2620cb7a2779045f2dcc3
fcheeley <35745104+fcheeley@users.noreply.github.com>
2018-01-27 23:47:17 -0500

    Update config.php

--- _php/config.php
+++ _php/config.php
@@ -3,13 +3,13 @@
 <?php
 	// $con and $server to be used throughout site
 	// ++++ Change: Added $server variable to make it easy to change reference paths.
-	$url = getenv('JAWSDB_MARIA_URL');
-	$dbparts = parse_url($url);
+	#//$url = getenv('JAWSDB_MARIA_URL');
+	#//$dbparts = parse_url($url);
 
-	$hostname = $dbparts['host'];
-	$username = $dbparts['user'];
-	$password = $dbparts['pass'];
-	$database = ltrim($dbparts['path'],'/');	
+	#//$hostname = $dbparts['host'];
+	#//$username = $dbparts['user'];
+	#//$password = $dbparts['pass'];
+	#//$database = ltrim($dbparts['path'],'/');	
 
 	// Lets try Katies DB first
 	// This should be simplified when this is placed into production to simply query 1 database
@@ -19,16 +19,16 @@
 
 
 	//connect to db
-	@$con = mysqli_connect($hostname, $username, $password, $database); 
+	#//@$con = mysqli_connect($hostname, $username, $password, $database); 
 	//-------------------------------------------
 
-	 if(mysqli_connect_errno()){ // failed to connect to katies database; lets try local host next
+	 #//if(mysqli_connect_errno()){ // failed to connect to katies database; lets try local host next
 		//-------------SERVER CONNECTION-------------
 		//connection info
-		$hostname = 'localhost:3306';
+		$hostname = 'mga-db.chdrdc01ua4v.us-east-2.rds.amazonaws.com';
 		$database = 'mga_db'; 
-		$username = 'user1';
-		$password = 'thisuser';
+		$username = 'fcheeley';
+		$password = 'fcheeley123';
 		$con = mysqli_connect($hostname, $username, $password, $database); 
 		if(mysqli_connect_errno()){  // failed to connect to local db too
 			die('Could not connect: ' . mysqli_connect_error());
