0d013e799c9f7c7a92c2b301744182a66c5aa2d7
fcheeley <35745104+fcheeley@users.noreply.github.com>
2018-02-02 23:37:01 -0500

    Update config.php
    Changed variable names for DB connect credentials due to conflict of interest with login variables

--- _php/config.php
+++ _php/config.php
@@ -1,42 +1,89 @@
 <html>
-<body>
-<?php
-	// $con and $server to be used throughout site
-	// ++++ Change: Added $server variable to make it easy to change reference paths.
-	$url = getenv('JAWSDB_MARIA_URL');
-	$dbparts = parse_url($url);
 
-	$hostname = $dbparts['host'];
-	$username = $dbparts['user'];
-	$password = $dbparts['pass'];
-	$database = ltrim($dbparts['path'],'/');	
+<body>
+
+<?php
+
+	// $con and $server to be used throughout site
+
+	// ++++ Change: Added $server variable to make it easy to change reference paths.
+
+	#//$url = getenv('JAWSDB_MARIA_URL');
+
+	#//$dbparts = parse_url($url);
+
+
+
+	#//$hostname = $dbparts['host'];
+
+	#//$username = $dbparts['user'];
+
+	#//$password = $dbparts['pass'];
+
+	#//$database = ltrim($dbparts['path'],'/');	
+
+
 
 	// Lets try Katies DB first
+
 	// This should be simplified when this is placed into production to simply query 1 database
+
 	
+
 	//-------------SERVER CONNECTION-------------
+
 	//connection info
 
 
+
+
+
 	//connect to db
-	@$con = mysqli_connect($hostname, $username, $password, $database); 
+
+	#//@$con = mysqli_connect($hostname, $username, $password, $database); 
+
 	//-------------------------------------------
 
-	 if(mysqli_connect_errno()){ // failed to connect to katies database; lets try local host next
+
+
+	 #//if(mysqli_connect_errno()){ // failed to connect to katies database; lets try local host next
+
 		//-------------SERVER CONNECTION-------------
+
 		//connection info
-		$hostname = 'localhost:3306';
-		$database = 'mga_db'; 
-		$username = 'user1';
-		$password = 'thisuser';
-		$con = mysqli_connect($hostname, $username, $password, $database); 
+
+		
+
+		$dbhost = 'mga-db.chdrdc01ua4v.us-east-2.rds.amazonaws.com';
+
+
+
+		$dbname = 'mga_db'; 
+
+
+
+		$dbuser = 'fcheeley';
+
+
+
+		$dbpass = 'fcheeley123';
+
+
+
+		$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);  
+
 		if(mysqli_connect_errno()){  // failed to connect to local db too
+
 			die('Could not connect: ' . mysqli_connect_error());
+
 		}
-		else {
-			echo "<script type = 'text/javascript'>alert('Online DB unavailable, connected to local DB');</script>";
-		}
-	 }
+
+		
+
+	 
+
 	?>
+
 </body>
+
 </html>
