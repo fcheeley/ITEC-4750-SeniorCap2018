730f73866568ea22f04d669885b1ce86ff4e4e42
fcheeley <35745104+fcheeley@users.noreply.github.com>
2018-02-12 21:37:40 -0500

    Update login.php
    test

--- login.php
+++ login.php
@@ -32,9 +32,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/_php/_objects/login_do.php');
 					<div class="form-group">
 						<div class="col-sm-offset-2 col-sm-9">
 							<input type="submit" value="Login" name="Login" class="btn btn-primary btn-lg submit"
-							       onclick="return confirm('By clicking Login, you agree to abide by the terms of the Middle Georgia State University IT 
-							and Electronic Communications Policy. A copy of this policy can be viewed on the intranet. The type of material you access
-							on the Internet could be monitored and filtered.')">
+							       onclick="return confirm('By clicking Login, you agree to abide by the terms of the Middle Georgia State University IT and Electronic Communications Policy. A copy of this policy can be viewed on the intranet. The type of material you access on the Internet could be monitored and filtered.')">
 						</div>
 					</div>
 					<!-- ++++ Change: Removed errorBlock handled in Login_DO rather than SESSION ++++ -->
